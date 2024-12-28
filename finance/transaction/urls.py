34c0813574f77c4f656de35820381aa60cc97df3
jvit1 <jpvithoulkas@gmail.com>
2024-12-28 18:28:42 -0500

    pretty clean submission

--- finance/transaction/urls.py
+++ finance/transaction/urls.py
@@ -5,4 +5,5 @@ app_name = 'transaction'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('update-category/<int:transaction_id>/', views.update_transaction_category, name='update-category'),
 ]
--- finance/transaction/views.py
+++ finance/transaction/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
-from transaction.models import Transaction
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from transaction.models import Transaction, Category
 
 
 def home(request):
     txn = Transaction.objects.all()
     context = {
-        'txn': txn
+        'txn': txn,
+        'categories': Category.objects.all()
     }
     return render(request, 'templates/transaction/transactions.html', context)
+
+def update_transaction_category(request, transaction_id):
+    transaction = get_object_or_404(Transaction, pk=transaction_id)
+    if request.method == 'POST':
+        category_id = request.POST.get('category')
+        category = get_object_or_404(Category, pk=category_id)
+        
+        transaction.category = category
+        transaction.save()
+        return HttpResponse(status=204)
+            
