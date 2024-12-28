59c4f3e87d5193f5a8987ce54f2c54da77588480
jvit1 <jpvithoulkas@gmail.com>
2024-12-28 17:24:03 -0500

    overview and base

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'finance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "templates" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
